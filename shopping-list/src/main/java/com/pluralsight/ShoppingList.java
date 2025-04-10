dcfebf11812ccf826af9f319c91a908ed9fe2ab5
DavidLisk88 <davidperrylisk@gmail.com>
2025-04-10 11:21:34 -0500

    completed 10 shopping list items

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("1. Milk");
+        System.out.println("2. Eggs");
+        System.out.println("3. Bread");
+        System.out.println("4. Butter");
+        System.out.println("5. Salt");
+        System.out.println("6. Pepper");
+        System.out.println("7. Mustard");
+        System.out.println("8. Mayo");
+        System.out.println("9. Water");
+        System.out.println("10. Chicken Breast");
+
+    }
+}
