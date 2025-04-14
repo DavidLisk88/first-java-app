37f2f2f96aa2c1d0ba73b89100130de80f89543b
DavidLisk88 <davidperrylisk@gmail.com>
2025-04-14 13:22:18 -0500

    Completed Mod3 - Exercise 3 - Basic Calculator

---
+++ BasicCalculator/src/main/java/BasicCalculator/BasicCalculator.java
@@ -0,0 +1,11 @@
+package BasicCalculator;
+import java.util.Scanner;
+public class BasicCalculator {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 12;
+        int result1 = num1 * num2;
+
+        System.out.println(result1);
+    }
+}
